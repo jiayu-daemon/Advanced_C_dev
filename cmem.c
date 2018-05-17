d5a09b0806155b5e9f9c9bb3010f4ed67a71ed1d
jiayu <jiayu_daemon@163.com>
2018-05-17 11:38:22 +0800

    co

--- cmem.c
+++ cmem.c
@@ -3,29 +3,44 @@
 #include <stdlib.h>
 
 
-char string[10][20] ={"hi", ",", "I","am","very","happy","to","hear","this","."};
-
-
 void get_memory(char **p, int size)
 {
-	*p = (char *)malloc(num);
+	*p = (char *)malloc(size); // 返回地址
 }
 
-void memory_free(char **p)
+void memory_free(char *p)
 {
-	free(*p);
+	free(p);
 }
+
+
+void swap(int * a, int * b)
+{
+	int t;
+	t = *a;
+	*a = *b;
+	*b = t;
+}
+
+
 
 int main(int argc, char const *argv[])
 {
 	/* code */
 	char * point = NULL;
+
 	get_memory(&point,10);
 	printf("0x%X\n",point);
+
 	strncpy(point,"hello",strlen("hello"));
 	printf("%s\n",point);
-	memory_free(&point);
+	
+	//free(point); // free 变量
+	memory_free(point);
 
+	int a = 3,b =6;
+	swap(&a,&b);
+	printf("a = %d b = %d\n",a,b);
 
 	return 0;
 }
